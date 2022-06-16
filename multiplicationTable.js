17a4561cde222da73354415db29846ccdf72f9b5
K. Virishapkov <105133869+Virishapkov@users.noreply.github.com>
2022-06-16 06:05:48 +0200

    Add files via upload

---
+++ multiplicationTable.js
@@ -0,0 +1,9 @@
+function multiplicationTable(input){
+    let number = Number(input[0])
+
+    for(let i = 1; i <= 10; i++){
+        let res = i * number
+        console.log(`${i} * ${number} = ${res}`)
+    }
+}
+multiplicationTable([5])
